da4e690b1d54ecb9c2cb64c08775ece224dce5e7
zaynsuh <tjworud123@gmail.com>
2026-09-02 00:29:01 +0900

    feat(routing): configure BrowserRouter and page routes
    - Why: enable navigation between home and login
    - What: add / and /login routes via react-router-dom
    - Affected: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomeLanding from './pages/HomeLanding'
+import Login from './pages/Login'
+
 function App() {
-  return null
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeLanding />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
